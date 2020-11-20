2ed7f0e27d683e5cfdfe0aca317163f298d6fef9
Airi-Aikawa <airi-aikawa@x-power.co.jp>
2020-11-20 09:41:33 +0900

    20201120_表示する文字を増やしました

--- src/components/Instruction.vue
+++ src/components/Instruction.vue
@@ -1,52 +1,374 @@
 <template>
-    <div>
-<p class="instruction">Instructions</p>
-<p class="rectangle"></p>
-<p class="rectangle1">1</p>
-    </div>
+  <div>
+    <p class="instruction">Instructions</p>
+    <p class="rectangle43"></p>
+    <p class="rectangle1">1</p>
+    <p class="oven">
+      Adjust the oven rack to the lower-middle position and preheat oven to
+      350°F (177°C).
+    </p>
+    <p class="rectangle44"></p>
+    <p class="rectangle2">2</p>
+    <p class="foodProcessor">
+      Make the crust: Using a food processor, pulse the graham crackers into
+      crumbs. Pour into a medium bowl and stir in sugar and melted butter until
+      combined. (You can also pulse it all together in the food processor.)
+      Mixture will be sandy. Press firmly into the bottom and slightly up the
+      sides of a 9-inch or 10-inch springform pan. No need to grease the pan
+      first. I use the bottom of a measuring cup to pack the crust down tightly.
+      Pre-bake for 8 minutes. Remove from the oven and place the hot pan on a
+      large piece of aluminum foil. The foil will wrap around the pan for the
+      water bath in step 4. Allow crust to slightly cool as you prepare the
+      filling.
+    </p>
+    <p class="rectangle45"></p>
+    <p class="rectangle3">3</p>
+    <p class="mixer">
+      Make the filling: Using a handheld or stand mixer fitted with a paddle
+      attachment, beat the cream cheese and granulated sugar together on
+      medium-high speed in a large bowl until the mixture is smooth and creamy,
+      about 2 minutes. Add the sour cream, vanilla extract, and lemon juice then
+      beat until fully combined. On medium speed, add the eggs one at a time,
+      beating after each addition until just blended. After the final egg is
+      incorporated into the batter, stop mixing. To help prevent the cheesecake
+      from deflating and cracking as it cools, avoid over-mixing the batter as
+      best you can.
+    </p>
+    <p class="rectangle46"></p>
+    <p class="rectangle4">4</p>
+    <p class="water">
+      Prepare the simple water bath (see note) Boil a pot of water. You need 1
+      inch of water in your roasting pan for the water bath, so make sure you
+      boil enough. I use an entire kettle of hot water. As the water is heating
+      up, wrap the aluminum foil around the springform pan. Pour the cheesecake
+      batter on top of the crust. Use a rubber spatula or spoon to smooth it
+      into an even layer. Place the pan inside of a large roasting pan.
+      Carefully pour the hot water inside of the pan and place in the oven. (Or
+      you can place the roasting pan in the oven first, then pour the hot water
+      in. Whichever is easier for you.)
+    </p>
+    <p class="rectangle47"></p>
+    <p class="rectangle5">5</p>
+    <p class="bake">
+      Bake cheesecake for 55-70 minutes or until the center is almost set. When
+      it’s done, the center of the cheesecake will slightly wobble if you gently
+      shake the pan. Turn the oven off and open the oven door slightly. Let the
+      cheesecake sit in the oven in the water bath as it cools down for 1 hour.
+      Remove from the oven and water bath, then cool cheesecake completely at
+      room temperature. Then refrigerate the cheesecake for at least 4 hours or
+      overnight.
+    </p>
+    <p class="rectangle48"></p>
+    <p class="rectangle6">6</p>
+    <p class="knife">
+      Use a knife to loosen the chilled cheesecake from the rim of the
+      springform pan, then remove the rim. Using a clean sharp knife, cut into
+      slices for serving. For neat slices, wipe the knife clean and dip into
+      warm water between each slice.
+    </p>
+    <p class="rectangle49"></p>
+    <p class="rectangle7">7</p>
+    <p class="serveCheesecake">
+      Serve cheesecake with desired toppings. Cover and store leftover
+      cheesecake in the refrigerator for up to 5 days.
+    </p>
+    <a
+      href="Source: https://sallysbakingaddiction.com/classic-cheesecake/ "
+      class="url"
+      >Source: https://sallysbakingaddiction.com/classic-cheesecake/
+    </a>
+    <footer class="footer">Your name @ DevChallenges.io</footer>
+  </div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
-.instruction{
-position: absolute;
-width: 137px;
-height: 32px;
-left: 236px;
-top: 1382px;
-font-family: Playfair Display;
-font-style: normal;
-font-weight: bold;
-font-size: 24px;
-line-height: 32px;
-color: #333333;
+.instruction {
+  position: absolute;
+  width: 137px;
+  height: 32px;
+  left: 236px;
+  top: 1382px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #333333;
 }
-.rectangle{
-position: absolute;
-width: 36px;
-height: 36px;
-left: 236px;
-top: 1442px;
-background: #F2994A;
-border-radius: 6px;
+.rectangle43 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 1442px;
+  background: #f2994a;
+  border-radius: 6px;
 }
-.rectangle1{
-position: absolute;
-width: 10px;
-height: 32px;
-left: 249px;
-top: 1440px;
-font-family: Playfair Display;
-font-style: normal;
-font-weight: bold;
-font-size: 24px;
-line-height: 32px;
-color: #FFFFFF;
+.rectangle1 {
+  position: absolute;
+  width: 10px;
+  height: 32px;
+  left: 249px;
+  top: 1440px;
+  font-family: "Playfair Display";
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
 }
-
+.oven {
+  position: absolute;
+  width: 528px;
+  height: 40px;
+  left: 287px;
+  top: 1442px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 20px;
+  color: #000000;
+}
+.rectangle44 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 1513px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle2 {
+  position: absolute;
+  width: 13px;
+  height: 32px;
+  left: 249px;
+  top: 1511px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.foodProcessor {
+  position: absolute;
+  width: 528px;
+  height: 264px;
+  left: 287px;
+  top: 1513px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #333333;
+}
+.rectangle45 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 1799px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle3 {
+  position: absolute;
+  width: 12px;
+  height: 32px;
+  left: 249px;
+  top: 1797px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.mixer {
+  position: absolute;
+  width: 528px;
+  height: 240px;
+  left: 287px;
+  top: 1799px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #333333;
+}
+.rectangle46 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 2077px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle4 {
+  position: absolute;
+  width: 13px;
+  height: 32px;
+  left: 248px;
+  top: 2074px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.water {
+  position: absolute;
+  width: 528px;
+  height: 240px;
+  left: 287px;
+  top: 2077px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #333333;
+}
+.rectangle47 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 2357px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle5 {
+  position: absolute;
+  width: 12px;
+  height: 32px;
+  left: 249px;
+  top: 2355px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.bake {
+  position: absolute;
+  width: 528px;
+  height: 192px;
+  left: 287px;
+  top: 2357px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #000000;
+}
+.rectangle48 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 2589px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle6 {
+  position: absolute;
+  width: 14px;
+  height: 32px;
+  left: 247px;
+  top: 2589px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.knife {
+  position: absolute;
+  width: 528px;
+  height: 96px;
+  left: 287px;
+  top: 2589px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #000000;
+}
+.rectangle49 {
+  position: absolute;
+  width: 36px;
+  height: 36px;
+  left: 236px;
+  top: 2723px;
+  background: #f2994a;
+  border-radius: 6px;
+}
+.rectangle7 {
+  position: absolute;
+  width: 11px;
+  height: 32px;
+  left: 249px;
+  top: 2721px;
+  font-family: Playfair Display;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 24px;
+  line-height: 32px;
+  color: #ffffff;
+}
+.serveCheesecake {
+  position: absolute;
+  width: 528px;
+  height: 48px;
+  left: 287px;
+  top: 2723px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #000000;
+}
+.url {
+  position: absolute;
+  width: 500px;
+  height: 24px;
+  left: 236px;
+  top: 2828px;
+  font-family: Montserrat;
+  font-style: italic;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 150%;
+  color: #bdbdbd;
+}
+.footer {
+  position: absolute;
+  width: 226px;
+  height: 17px;
+  left: 597px;
+  top: 2952px;
+  font-family: Montserrat;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 14px;
+  line-height: 17px;
+  color: #a9a9a9;
+  white-space: nowrap;
+}
 </style>
